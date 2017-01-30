7d600c6c193376db159a88f8e1e2a154709fbc1e
waakye <dailyomnivore@gmail.com>
2017-01-30 23:37:27 +0800

    Add dummy data of books to MainActivity

--- app/src/main/java/waakye/booklistingapp/MainActivity.java
+++ app/src/main/java/waakye/booklistingapp/MainActivity.java
@@ -2,6 +2,10 @@ package waakye.booklistingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a fake list of books
+        ArrayList<String> books = new ArrayList<>();
+        books.add("Moby Dick");
+        books.add("Tale of Two Cities");
+        books.add("Bridget Jones Diary");
+        books.add("Silence of the Lambs");
+        books.add("The Bible");
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView bookListView = (ListView) findViewById(R.id.list);
+
+        // Create a new {@link ArrayAdapter} of books
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, books);
+
+        // Set the adapter on the {@link ListView} so the list can be populated in the UI
+        bookListView.setAdapter(adapter);
     }
 }
